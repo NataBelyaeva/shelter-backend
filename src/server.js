bfc66a6bb93046f1687d6f91c9ff31f44064a6ae
NataBelyaeva <belyaevanata04@gmail.com>
2026-06-10 23:44:12 +0700

    retern

--- src/server.js
+++ src/server.js
@@ -3,11 +3,9 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 // Делаем папку public/uploads доступной статически
 app.use('/uploads', express.static('public/uploads'));
